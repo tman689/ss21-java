d8031a6034462470e35ea54e6421460594476c09
tman689 <tranminhan05092000@gmail.com>
2025-03-25 20:54:00 +0700

    Update main.js

--- ss21/bt5/main.js
+++ ss21/bt5/main.js
@@ -1,9 +1,9 @@
 for (let i = 1; i <= 100; i++){
-    if (i % 3 === 0 && i % 5 === 0){
+    if (i % 3 == 0 && i % 5 == 0){
         document.writeln("FizzBuzz"); 
-    }else if (i % 3 === 0){
+    }else if (i % 3 == 0){
         document.writeln("Fizz");
-    }else if (i % 5 === 0){
+    }else if (i % 5 == 0){
         document.writeln("Buzz");
     }else{
         document.writeln(i); 
